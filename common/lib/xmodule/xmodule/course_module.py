f82f1a8509aa7b7294720b630c80b0f8697816f8
Ned Batchelder <ned@edx.org>
2015-03-31 12:05:43 -0400

    Put base classes in most-specific to most-general order.
    Also remove LicenseMixin from the cms all-xblocks mixins.

--- common/lib/xmodule/xmodule/course_module.py
+++ common/lib/xmodule/xmodule/course_module.py
@@ -856,7 +856,7 @@ class CourseModule(CourseFields, SequenceModule):  # pylint: disable=abstract-me
     """
 
 
-class CourseDescriptor(CourseFields, SequenceDescriptor, LicenseMixin):
+class CourseDescriptor(CourseFields, LicenseMixin, SequenceDescriptor):
     """
     The descriptor for the course XModule
     """
