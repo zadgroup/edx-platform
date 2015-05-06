4082ceb3c99b1c2ab5e6b2b5fa4e6679bde3adfd
Ahsan Ulhaq <ahsan.haq@arbisoft.com>
2015-05-06 14:36:46 +0500

    Comboboxes on Learner Profile page have an onchange listener
    AC-104

--- lms/static/js/views/fields.js
+++ lms/static/js/views/fields.js
@@ -304,7 +304,6 @@
 
             events: {
                 'click': 'startEditing',
-                'change select': 'finishEditing',
                 'focusout select': 'finishEditing'
             },
 
@@ -312,7 +311,7 @@
                 _.bindAll(this, 'render', 'optionForValue', 'fieldValue', 'displayValue', 'updateValueInField', 'saveValue');
                 this._super(options);
 
-                this.listenTo(this.model, "change:" + this.options.valueAttribute, this.updateValueInField);
+                this.listenTo(this.model, "focusout:" + this.options.valueAttribute, this.updateValueInField);
             },
 
             render: function () {
